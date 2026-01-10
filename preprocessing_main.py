ed735c19ea748d4c6038d3ca13392fee0be51653
Alexej Nikulin <alexej.nikulin@patientzero-games.com>
2026-01-10 14:00:46 +0100

    Vocabulary fixes

--- preprocessing_main.py
+++ preprocessing_main.py
@@ -1,6 +1,6 @@
-# 1 - Extract_patient_level_events
-# 2 - Sort_and_merge
-# 3 - Split_combined
+# 1 - Extract_patient_level_events - WORKS
+# 2 - Sort_and_merge - WORKS
+# 3 - Split_combined - WORKS
 # 4 - Vocabulary
 # 5 - Build_patient_event_sequences
 # 6 - tokenize_sequences
