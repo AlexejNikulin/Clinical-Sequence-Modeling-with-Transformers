c41e0f653c6661153e5d409c6ba58808d5b9520d
Alexej Nikulin <alexej.nikulin@patientzero-games.com>
2026-01-17 12:47:18 +0100

    Revert "Merge remote-tracking branch 'origin/main' into creating_main_script"
    This reverts commit 38fe38b75171f83b02fd6449b2a1a66706a48bb9, reversing
    changes made to f262e4bad6b3d9b0bbc45fc854799cde469a2527.

--- preprocessing_main.py
+++ preprocessing_main.py
@@ -8,30 +8,30 @@ from pathlib import Path
 from transformer.transformer_train_model import Transformer
 import pandas as pd
 
-# # 1 - Extract_patient_level_events - WORKS
+# # 1 - Extract_patient_level_events 
 # patientLevelEventExtractor = PatientLevelEventExtractor()
 # patientLevelEventExtractor.start_extraction()
 
-# # 2 - Sort_and_merge - WORKS
+# # 2 - Sort_and_merge
 # sortMerger = SortMerger()
 # sortMerger.sort_and_merge()
 
-# # 3 - Split_combined - WORKS
+# # 3 - Split_combined 
 # dataSplitter = DataSplitter()
 # dataSplitter.split_dataset() 
 
-# # 4 - Vocabulary - WORKS
+# # 4 - Vocabulary
 # vocabulary = Vocabulary()
 # vocabulary.build_vocabulary()
 
-# # 5 - Build_patient_event_sequences - WORKS
+# # 5 - Build_patient_event_sequences
 # COMBINED_CSV = Path("../out/splits_out/combined_train.csv")
 # df = pd.read_csv(COMBINED_CSV)
 
-# eventSequencer = EventSequencer()
+# eventSequencer = EventSequencer() 
 # sequences = eventSequencer.build_patient_event_sequences(df)
 
-# # 6 - tokenize_sequences - WORKS
+# # 6 - tokenize_sequences
 # tokenSequencer = TokenSequencer()
 # ids = tokenSequencer.build_sequences(sequences)
 
