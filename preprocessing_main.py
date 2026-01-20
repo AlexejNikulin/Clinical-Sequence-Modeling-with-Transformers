cba029c588c83a5d5b6dad31016d9366e71bc89d
Alexej Nikulin <alexej.nikulin@patientzero-games.com>
2026-01-20 19:34:18 +0100

    Reset main

--- preprocessing_main.py
+++ preprocessing_main.py
@@ -18,44 +18,44 @@ def main():
     # # extractor = PatientLevelEventExtractor()
     # # extractor.start_extraction()
 
-    # Short version for debugging
-    extractor = PatientLevelEventExtractor_Short()
-    extractor.start_extraction()
+    # # Short version for debugging
+    # extractor = PatientLevelEventExtractor_Short()
+    # extractor.start_extraction()
 
-    # 2) Sort and merge events
-    sort_merger = SortMerger()
-    sort_merger.sort_and_merge()
+    # # 2) Sort and merge events
+    # sort_merger = SortMerger()
+    # sort_merger.sort_and_merge()
 
-    # 2.1) Add time tokens
-    COMBINED_CSV = Path("../out/merge_and_sort/combined.csv")
-    df = pd.read_csv(COMBINED_CSV)
+    # # 2.1) Add time tokens
+    # COMBINED_CSV = Path("../out/merge_and_sort/combined.csv")
+    # df = pd.read_csv(COMBINED_CSV)
 
-    event_sequencer = EventSequencer()
-    sequences = event_sequencer.add_time_tokens_to_data(df)
+    # event_sequencer = EventSequencer()
+    # sequences = event_sequencer.add_time_tokens_to_data(df)
 
-    # 3) Train/val/test split
-    splitter = DataSplitter()
-    splitter.split_dataset()
+    # # 3) Train/val/test split
+    # splitter = DataSplitter()
+    # splitter.split_dataset()
 
-    # 4) Build vocabulary
-    vocab = Vocabulary()
-    vocab.build_vocabulary()
+    # # 4) Build vocabulary
+    # vocab = Vocabulary()
+    # vocab.build_vocabulary()
 
-    # 5) Build patient event sequences
-    for COMBINED_CSV, OUT_IDS in [
-        ("../out/splits_out/combined_train.csv", "ids.json"),
-        ("../out/splits_out/combined_val.csv", "val_ids.json"),
-    ]:
-        df = pd.read_csv(COMBINED_CSV)
-        event_sequencer = EventSequencer()
-        sequences = event_sequencer.build_patient_event_sequences(df, vocab)
-        del df
+    # # 5) Build patient event sequences
+    # for COMBINED_CSV, OUT_IDS in [
+    #     ("../out/splits_out/combined_train.csv", "ids.json"),
+    #     ("../out/splits_out/combined_val.csv", "val_ids.json"),
+    # ]:
+    #     df = pd.read_csv(COMBINED_CSV)
+    #     event_sequencer = EventSequencer()
+    #     sequences = event_sequencer.build_patient_event_sequences(df, vocab)
+    #     del df
 
-        # 6) Tokenize sequences
-        token_sequencer = TokenSequencer()
-        out_dir = Path("../out/sequences")
-        out_dir.mkdir(parents=True, exist_ok=True)
-        token_sequencer.build_sequences(sequences, False, out_dir / OUT_IDS)
+    #     # 6) Tokenize sequences
+    #     token_sequencer = TokenSequencer()
+    #     out_dir = Path("../out/sequences")
+    #     out_dir.mkdir(parents=True, exist_ok=True)
+    #     token_sequencer.build_sequences(sequences, False, out_dir / OUT_IDS)
 
 if __name__ == "__main__":
     main()
